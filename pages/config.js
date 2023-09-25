cc3681ca79daf14bcab377861ec322b85adac3aa
xennony <dnkras@mail.ru>
2023-09-25 10:55:25 +0300

    Delete config

--- pages/config.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-console.log("process.env.SUPABASE_URL: " + process.env.NEXT_PUBLIC_SUPABASE_URL);
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_API_KEY;
-
-module.exports = createClient(supabaseUrl, supabaseAnonKey);
